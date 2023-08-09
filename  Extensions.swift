f6d8e326ea9f88a4252cab08fcf0f63fdb7cac00
Sabina Barinova <80158784+sabinabarinova@users.noreply.github.com>
2023-08-09 21:58:38 +0600

    Add files via upload

---
+++  Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  Extensions.swift
+//  Rec
+//
+//  Created by Adil Meirambek on 24.04.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension UIApplication {
+    
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
